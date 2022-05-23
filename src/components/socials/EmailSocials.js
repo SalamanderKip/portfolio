5943a86325b2b5fa1d2bfb3ca2321c25147b5d52
salamanderkip <200507@student.glu.nl>
2022-05-23 14:11:11 +0200

    email update

--- src/components/socials/EmailSocials.js
+++ src/components/socials/EmailSocials.js
@@ -5,7 +5,7 @@ export const EmailSocials = () => {
     return (
         <div className='socialsEmail'>
             <div className='socialEmailWrapper'>
-                <a href='mailto:info@bosmaarten.nl'>info@bosmaarten.nl</a>
+                <a href='mailto:bosmaarteninfo@gmail.com'>bosmaarteninfo@gmail.com</a>
             </div>
         </div>
     )
